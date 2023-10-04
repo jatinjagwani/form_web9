d4069cd10939dd600f5b144e1b17aacdd1111e72
Rajkishor <115188008+rizwiz215@users.noreply.github.com>
2023-10-04 13:15:38 +0530

    Update javascript.js

--- servey_form/javascript.js
+++ servey_form/javascript.js
@@ -1,3 +1,27 @@
-const a = document.getElementsByClassName("title2");
-a.classList.add('ab');
-console.log(a);
+ function fetchData() {
+      // Define the API URL you want to request data from
+      const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+
+      // Make a GET request to the API using the fetch function
+      fetch(apiUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json(); // Parse the response as JSON
+        })
+        .then(data => {
+          // Handle the JSON data
+          const outputElement = document.getElementById('output');
+          outputElement.innerHTML = `
+            <p>User ID: ${data.userId}</p>
+            <p>Post ID: ${data.id}</p>
+            <p>Title: ${data.title}</p>
+            <p>Body: ${data.body}</p>
+          `;
+        })
+        .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+        });
+    }
+ 
